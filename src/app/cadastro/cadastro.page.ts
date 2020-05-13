4bfe33fa8fd05f14c3a2a734b771f2e68b5f6cfc
Alline Oliveira allineo <alline.oliveira@gmail.com>
2020-05-13 13:00:31 -0300

    ts

--- src/app/cadastro/cadastro.page.ts
+++ src/app/cadastro/cadastro.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-cadastro',
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroPage implements OnInit {
 
-  constructor() { }
+  contactField: null;
+  messageField: null;
+
+  constructor(
+    private alertController: AlertController
+  ) { }
 
   ngOnInit() {
   }
 
+
+  async sendForm(f: NgForm) {
+
+    const message = 'Contato: ' + this.contactField +
+                    '<br>Mensagem:' + this.messageField;
+
+    const alert = await this.alertController.create({
+      header: 'Alerta!',
+      message,
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Canceled');
+          }
+        }, {
+          text: 'Ok',
+          handler: () => {
+            console.log('Alert Confirmed');
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
 }
+
